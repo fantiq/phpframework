10f5389b0727c68bfd9e66cc19b4f2c3dbef02c0
fanyilong <fanyilong_v5@sina.com>
2015-06-07 07:32:38 +0800

    debug class tags

--- src/debug/Debug.php
+++ src/debug/Debug.php
@@ -6,6 +6,9 @@ namespace framework\debug;
  * 调试工具组件
  * @date 2015-01-25
  * @author vito_v5
+ *
+ * 这是一个好的想法，但是这个class可能会被移除 调试的输入输出会
+ * 移动到输出流里面实现
  */
 class Debug {
 
@@ -14,10 +17,11 @@ class Debug {
      * @param mix $obj
      */
     public static function p($obj) {
-        if (is_array($obj))
+        if (is_array($obj)){
             echo '<pre>' . print_r($obj,true) . '</pre>';
-        else
-            echo $obj;
+        }else{
+		    echo $obj;
+		}
     }
     
 }
